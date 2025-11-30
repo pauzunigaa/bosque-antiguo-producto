7ddd292cc6ab7a1a1ae2b447e065844e2edd5852
cmongez <camongez28@gmail.com>
2025-11-30 16:14:32 -0300

    Fix en modleo producto

--- src/main/java/Producto/Producto/model/Producto.java
+++ src/main/java/Producto/Producto/model/Producto.java
@@ -38,14 +38,7 @@ public class Producto {
     @Column(nullable = false)
     private Integer stockCritico;
 
-    @Column(nullable = false)
-    private Integer stock;
-
-    @Column(nullable = false)
-    private Integer stockCritico;
-
     @ManyToOne(optional = false, fetch = FetchType.EAGER)
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
-
 }
